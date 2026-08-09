fabd65a35331f210de911088b03830c7d5348b49
Sourav Ranjan Samal <samalsouravranjan@gmail.com>
2026-08-09 19:47:07 +0530

    Time: 5 ms (51.47%) | Memory: 19.1 MB (98.46%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,21 +1,30 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        values = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-        }
+        symbols = [
+            "I", "V", "X", "L",
+            "C", "D", "M"
+        ]
+
+        values = [
+            1, 5, 10, 50,
+            100, 500, 1000
+        ]
 
         result = 0
+        i = 0
 
-        for i in range(len(s)):
-            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
-                result -= values[s[i]]
-            else:
-                result += values[s[i]]
+        while i < len(s):
+            current = symbols.index(s[i])
+
+            if i + 1 < len(s):
+                next = symbols.index(s[i + 1])
+
+                if values[current] < values[next]:
+                    result += values[next] - values[current]
+                    i += 2
+                    continue
+
+            result += values[current]
+            i += 1
 
         return result
